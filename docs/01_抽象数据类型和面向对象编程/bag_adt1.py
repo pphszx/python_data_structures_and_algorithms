8be213e12d4f3af3cc8f902cf8393f891b0d5bd9
pphszx <pphszx@gmail.com>
2020-11-24 16:29:17 +0800

    01

---
+++ docs/01_抽象数据类型和面向对象编程/bag_adt1.py
@@ -0,0 +1,35 @@
+class Bag(object):
+    """
+    背包类型
+    """
+    def __init__(self, maxsize=10):
+        self.maxsize = maxsize
+        self._items = []
+
+    def add(self, item):
+        if len(self._items) < self.maxsize:
+            self._items.append(item)
+
+    def remove(self, item):
+        self._items.remove(item)
+
+    def __len__(self):
+        return len(self._items)
+
+    def __iter__(self):
+        yield self._items
+
+
+
+def test_bag():
+    bag = Bag(4)
+    bag.add(1)
+    bag.add(2)
+    bag.add(3)
+    assert(len(bag)==3)
+    bag.remove(2)
+    assert(len(bag)==2)
+
+
+if __name__ == "__main__":
+    test_bag()
